e200d05295f02436f2be254771fa55e00cf510cb
Ashwin M A <ashwin.magalu@gmail.com>
2024-09-22 19:00:23 +0530

    Making dist folder visible
    I have changed the command and made the dist folder visible for reference

--- src/components/HMR.jsx
+++ src/components/HMR.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function HMR() {
-  return <div>Webpack Hot Module replacement example!</div>;
+  return (
+    <div>
+      <h1>Webpack Hot Module replacement example!</h1>
+      <input placeholder="type anyting" />
+    </div>
+  );
 }
 
 export default HMR;
